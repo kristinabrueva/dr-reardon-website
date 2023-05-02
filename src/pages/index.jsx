64c4e5604840c2223b4fcc536e3d59a552eb8b92
Kristina Brueva <kristinabruuu@gmail.com>
2023-05-02 12:12:07 +1000

    Show articles on home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -283,11 +283,11 @@ export default function Home({ articles }) {
       <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          {/* <div className="flex flex-col gap-16">
+          <div className="flex flex-col gap-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
-          </div> */}
+          </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Newsletter />
             <Resume />
