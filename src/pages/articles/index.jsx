024c65d05f96ee115a67c22d2e7a45beef81d65d
Kristina Brueva <kristinabruuu@gmail.com>
2023-05-02 12:11:12 +1000

    Update articles

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,16 +38,10 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
-        <meta
-          name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-        />
+        <title>Articles - Daniel Reardon</title>
+        <meta name="description" content="Articles by Daniel Reardon" />
       </Head>
-      <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-      >
+      <SimpleLayout>
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
