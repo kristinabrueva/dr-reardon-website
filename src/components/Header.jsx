18691c196c76e1ae5cc28a8fbae1c7e0cb79c82c
Kristina Brueva <kristinabruuu@gmail.com>
2023-05-03 16:32:59 +1000

    Remove About link from mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -121,7 +121,7 @@ function MobileNavigation(props) {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
+                <MobileNavItem href="/">Home</MobileNavItem>
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
                 <MobileNavItem href="/speaking">Speaking</MobileNavItem>
